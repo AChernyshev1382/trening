33e80b3ba4d376772b5b6186f7f2ee43be892d0d
CHERNYSHEV Artem <artem.chernyshev@raiffeisen.ru>
2018-09-28 17:05:03 +0300

    add new class GitBox

---
+++ untitled/src/giftbox/GiftBox.java
@@ -0,0 +1,19 @@
+package giftbox;
+
+
+
+
+
+
+public class GiftBox {
+
+    double totalPrice;
+    double totalWeight;
+    String swetName;
+
+    public GiftBox(double totalPrice, double totalWeight, String swetName) {
+        this.totalPrice = totalPrice;
+        this.totalWeight = totalWeight;
+        this.swetName = swetName;
+    }
+}
