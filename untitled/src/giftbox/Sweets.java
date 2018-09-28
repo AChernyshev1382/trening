6e9ef6c6ad872fe77c6033d0bc77c61e82d0befe
CHERNYSHEV Artem <artem.chernyshev@raiffeisen.ru>
2018-09-28 17:12:16 +0300

    add new class Sweets

---
+++ untitled/src/giftbox/Sweets.java
@@ -0,0 +1,32 @@
+package giftbox;
+
+public class Sweets {
+
+    double price;
+    double weight;
+    String uniqProperty;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public String getUniqProperty() {
+        return uniqProperty;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setUniqProperty(String uniqProperty) {
+        this.uniqProperty = uniqProperty;
+    }
+}
